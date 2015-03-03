a310c5455ec69a5acb9edeed3fb5b2434bd76268
Brian Whicheloe <brian@Brians-MacBook-Pro.local>
2015-03-02 21:07:31 -0800

    Initial Commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="assets/dist/all.min.css">
+	 <meta charset="UTF-8">
+</head>
+<body>
+
+	<canvas id="canvas" width="5000" height="5000"></canvas>
+
+	test
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="assets/js/script.js"></script>
+</body>
+</html>
